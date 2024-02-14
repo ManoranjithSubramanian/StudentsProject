180aa5776e650562e3a3d66874866c50d8b8ce7e
ManoranjithSubramanian <manoranjith711@gmail.com>
2024-02-15 00:50:21 +0530

    Changed a line for commiting

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	//Added By Mano
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
